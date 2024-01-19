4bc137e886b6a28343db1d3af61bdb6d7c6b6047
MakerDZ <mcskb91@gmail.com>
2024-01-20 00:28:31 +0630

    refactor | testing for scroll position value

--- src/hooks/Scroll.ts
+++ src/hooks/Scroll.ts
@@ -5,10 +5,10 @@ function useIsScrolled(){
     const [isScrolled , setScrolled] = useState<boolean>(false);
     const { scrollY } = useScroll();
     useMotionValueEvent(scrollY,"change", (latest) => {
-        if(latest >= 300){
+        if(latest >= 100){
             setScrolled(true);
         }
-        if(latest < 300){
+        if(latest < 100){
             setScrolled(false);
         }
         console.log(isScrolled);
